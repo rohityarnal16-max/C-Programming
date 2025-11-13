0687a2f94a34f4874e312f4ee81854a4d2b98454
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:46:55 +0530

    Add files via upload

---
+++ C program for Conditional Operator.c
@@ -0,0 +1,18 @@
+// C program for Conditional Operator
+#include <stdio.h>
+
+int main() {
+    int a, b, max;
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Using conditional (ternary) operator to find the largest number
+    max = (a > b) ? a : b;
+
+    // Display result
+    printf("\nThe largest number is: %d\n", max);
+
+    return 0;
+}
