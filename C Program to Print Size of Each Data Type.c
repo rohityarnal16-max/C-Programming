0e5f1fc9731535bb67b1c1c4681ab067312f760e
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:31:33 +0530

    Update C Program to Print Size of Each Data Type.c

--- C Program to Print Size of Each Data Type.c
+++ C Program to Print Size of Each Data Type.c
@@ -1,6 +1,5 @@
 //C Program to Print Size of Each Data Type
-#include <stdio.h>
-
+#include <stdio.h>
 int main() {
     printf("Size of char: %lu bytes\n", sizeof(char));
     printf("Size of int: %lu bytes\n", sizeof(int));
