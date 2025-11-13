27027ea949888dc846502c701fe82f6d311face0
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 20:01:06 +0530

    Add files via upload

---
+++ Program to calculate the sum of first n natural number  do- while loop.c
@@ -0,0 +1,21 @@
+//  Program to calculate the sum of first n natural number -do-while loop
+#include <stdio.h>
+
+int main() {
+    int n, i = 1, sum = 0;
+
+    // Taking input from user
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Using do-while loop to calculate sum
+    do {
+        sum = sum + i;
+        i++;
+    } while (i <= n);
+
+    // Displaying result
+    printf("The sum of first %d natural numbers is: %d\n", n, sum);
+
+    return 0;
+}
