1fa0d17908ea07a82c7e5ef0be617edd8148bbd0
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:56:13 +0530

    Add files via upload

---
+++ Nested if  positivity of two numbers.c
@@ -0,0 +1,24 @@
+//// Nested if: positivity of two numbers
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > 0) {
+        if (b > 0)
+            printf("Both numbers are positive.\n");
+        else
+            printf("First number is positive, second is not positive.\n");
+    }
+    else {
+        if (b > 0)
+            printf("First number is not positive, second is positive.\n");
+        else
+            printf("Both numbers are not positive.\n");
+    }
+
+    return 0;
+}
