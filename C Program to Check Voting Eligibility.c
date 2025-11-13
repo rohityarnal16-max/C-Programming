6e902af2e71155224f3f70ba66b274cf6e0ebb3f
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:52:27 +0530

    Add files via upload

---
+++ C Program to Check Voting Eligibility.c
@@ -0,0 +1,21 @@
+// C Program to Check Voting Eligibility
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    // Input age
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    // Check voting eligibility
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    }
+    else {
+        printf("You are not eligible to vote.\n");
+        printf("You can vote after %d years.\n", 18 - age);
+    }
+
+    return 0;
+}
