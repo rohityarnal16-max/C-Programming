6b9553283081185910e9e670d1cf27835f4f6958
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:58:33 +0530

    Add files via upload

---
+++ C Program Calculator using switch case.c
@@ -0,0 +1,44 @@
+// C Program- Calculator using switch case
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Taking input from user
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Performing operation based on user choice
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error! Division by zero is not allowed.\n");
+                return 1;
+            }
+            printf("Result: %.2lf\n", result);
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
