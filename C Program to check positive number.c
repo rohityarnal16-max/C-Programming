4b4afde96a69f4956e3eb0e2d4c262454d96a986
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:50:19 +0530

    Add files via upload

---
+++ C Program to check positive number.c
@@ -0,0 +1,23 @@
+//  C Program to check positive number
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check condition
+    if (num > 0) {
+        printf("%d is a positive number.\n", num);
+    }
+    else if (num < 0) {
+        printf("%d is a negative number.\n", num);
+    }
+    else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
