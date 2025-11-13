7ebfe376c67d39d0ced238a8a39a6d9bf01633eb
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 19:01:39 +0530

    Add files via upload

---
+++ C program to display different data types.c
@@ -0,0 +1,17 @@
+// C program to display different data types
+#include <stdio.h>
+
+int main() {
+    int a = 5;             // integer type
+    float b = 5.0;          // float type
+    char c = 'A';           // character type
+    double d = 12.3456;     // double type
+
+    // Displaying the values of different data types
+    printf("Integer value: %d\n", a);
+    printf("Float value: %.2f\n", b);
+    printf("Character value: %c\n", c);
+    printf("Double value: %.4lf\n", d);
+
+    return 0;
+}
