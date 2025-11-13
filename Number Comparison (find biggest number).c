f7e91cf25baaab3fb7c2b3c6550deacf07c96388
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 20:09:41 +0530

    Add files via upload

---
+++ Number Comparison (find biggest number).c
@@ -0,0 +1,20 @@
+//  Number Comparison (find biggest number)
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Taking input from user
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Comparing numbers
+    if (num1 > num2)
+        printf("%d is the biggest number.\n", num1);
+    else if (num2 > num1)
+        printf("%d is the biggest number.\n", num2);
+    else
+        printf("Both numbers are equal.\n");
+
+    return 0;
+}
