0fe43ea57c2d4c5ca1a28a84fcac8821bb1fcac7
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-13 18:57:04 +0530

    Add files via upload

---
+++ C program for output statement (printf).c
@@ -0,0 +1,7 @@
+// C program for output statement (printf)
+#include <stdio.h>  // header file for input/output
+
+int main() {
+    printf("Hello world");  // output statement
+    return 0;  // end of program
+}
