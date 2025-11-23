b6a1da283209e20117d50319d53c7e4a495ddbc4
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-23 18:14:34 +0530

    Update C Program for swapping of two numbers.c

--- C Program for swapping of two numbers.c
+++ C Program for swapping of two numbers.c
@@ -3,19 +3,19 @@
 int main() {
     int a, b, temp;
 
-    // Input two numbers
+    
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    // Display before swapping
+    
     printf("\nBefore swapping: a = %d, b = %d\n", a, b);
 
-    // Swap using a temporary variable
+    
     temp = a;
     a = b;
     b = temp;
 
-    // Display after swapping
+    
     printf("After swapping: a = %d, b = %d\n", a, b);
 
     return 0;
