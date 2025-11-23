5e58fd2d3f7acf85994a5574a8cd9cf1b07acc90
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-23 18:32:49 +0530

    Update Factorial of  a number.c

--- Factorial of  a number.c
+++ Factorial of  a number.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 int main() {
     int n, i;
-    unsigned long long factorial = 1; // To handle large results
+    unsigned long long factorial = 1; 
 
-    // Taking input from user
+    
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    // Checking for negative input
+    
     if (n < 0)
         printf("Factorial of a negative number doesn't exist.\n");
     else {
-        // Calculating factorial using for loop
+        
         for (i = 1; i <= n; ++i) {
             factorial *= i;
         }
