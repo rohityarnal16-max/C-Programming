d1023449a125a5e8193f81f2dbc0ad6d55d97c76
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-23 18:19:07 +0530

    Update C Program to check even or odd.c

--- C Program to check even or odd.c
+++ C Program to check even or odd.c
@@ -3,11 +3,11 @@
 int main() {
     int num;
 
-    // Taking input from user
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Checking even or odd
+    
     if (num % 2 == 0)
         printf("%d is an even number.\n", num);
     else
