cb189a04e7af083560f22182fb69ba9f4d403f2c
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-23 18:35:39 +0530

    Update Program to calculate the sum of first n natural  number for loop.c

--- Program to calculate the sum of first n natural  number for loop.c
+++ Program to calculate the sum of first n natural  number for loop.c
@@ -3,16 +3,16 @@
 int main() {
     int n, i, sum = 0;
 
-    // Taking input from user
+    
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    // Using for loop to calculate sum
+    
     for (i = 1; i <= n; i++) {
         sum = sum + i;
     }
 
-    // Displaying result
+    
     printf("The sum of first %d natural numbers is: %d\n", n, sum);
 
     return 0;
