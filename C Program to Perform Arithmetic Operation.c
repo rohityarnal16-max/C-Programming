a6ea588f75d19d20ef81d616ed50a1ec8af53a7b
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-23 18:17:26 +0530

    Update C Program to Perform Arithmetic Operation.c

--- C Program to Perform Arithmetic Operation.c
+++ C Program to Perform Arithmetic Operation.c
@@ -5,18 +5,18 @@ int main() {
     int sum, diff, prod, mod;
     float div;
 
-    // Input two numbers from user
+    
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    // Perform arithmetic operations
+    
     sum = a + b;
     diff = a - b;
     prod = a * b;
     div = (float)a / b;  // typecast to get decimal result
     mod = a % b;
 
-    // Display results
+  
     printf("\nResults:\n");
     printf("Addition: %d\n", sum);
     printf("Subtraction: %d\n", diff);
