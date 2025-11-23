88e21ff9a018c381b786fd73cd0c512ee696a7e0
rohityarnal16-max <rohityarnal16@gmail.com>
2025-11-23 18:23:58 +0530

    Update C program  for Logical Operator.c

--- C program  for Logical Operator.c
+++ C program  for Logical Operator.c
@@ -3,17 +3,17 @@
 int main() {
     int a, b;
 
-    // Input two numbers
+    
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    // Display logical operation results
+    
     printf("\nResults of Logical Operations:\n");
 
-    printf("(a && b) : %d\n", (a && b));   // Logical AND
-    printf("(a || b) : %d\n", (a || b));   // Logical OR
-    printf("(!a)     : %d\n", (!a));       // Logical NOT for a
-    printf("(!b)     : %d\n", (!b));       // Logical NOT for b
+    printf("(a && b) : %d\n", (a && b));   
+    printf("(a || b) : %d\n", (a || b));   
+    printf("(!a)     : %d\n", (!a));       
+    printf("(!b)     : %d\n", (!b));       
 
     return 0;
 }
